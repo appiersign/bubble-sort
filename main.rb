1b44e4f7713f58b0e6f42541568865212a2515ad
aulbourn knowles <aulbourn@gmail.com>
2020-03-20 13:09:31 -0400

    started bubble_sort function

--- main.rb
+++ main.rb
@@ -1,1 +1,20 @@
-require 'rubygems'
+
+
+def bubble_sort(array)
+  n = array.length - 1
+
+  loop do
+    if_swap = true
+    0.upto(n) do |x|
+      if_swap = false
+      if array[x] > array[x + 1]
+        array[x], array[x + 1] = array[x + 1], array[x]
+        if_swap = true
+      end
+    end
+    break unless if_swap
+  end
+end
+
+arr = [4,3,78,2,0,2]
+print bubble_sort(arr)
