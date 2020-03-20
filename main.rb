fc4d056ab615f3723aa03a52c530bf0da63a5078
Solomon APPIER-SIGN <braasig@gmail.com>
2020-03-20 18:30:08 +0000

    added yield to bubble_sort_by

--- main.rb
+++ main.rb
@@ -14,14 +14,24 @@ def bubble_sort(array)
   array
 end
 
-arr = [4, 3, 78, 2, 0, 2]
+# arr = [4, 3, 78, 2, 0, 2]
 # print bubble_sort(arr)
 
 def bubble_sort_by(array)
-  yield
+  n = array.length - 1
+  n.times do
+    0.upto(n) do |x|
+      array[x], array[x + 1] = array[x + 1], array[x] if yield(array[x], array[x + 1]) == 1
+    end
+  end
+  print array
 end
 
-string_array = ["hi", "hey", "hello"]
-yield_block = string_array.each do |a, b| a <=> b end
+bubble_sort_by [2, 1, 4, 3] do |x, y|
+  x <=> y
+end
 
-bubble_sort_by($yield_block)
+# string_array = ["hi", "hey", "hello"]
+# yield_block = string_array.each do |a, b| a <=> b end
+#
+# bubble_sort_by(&yield_block)
