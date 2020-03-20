2a4d0836e25eca4de4c65a3ecbdeb928a6058027
Solomon APPIER-SIGN <braasig@gmail.com>
2020-03-20 18:42:45 +0000

    added checks for nil

--- main.rb
+++ main.rb
@@ -3,25 +3,26 @@ def bubble_sort(array)
 
   n.times do
     0.upto(n) do |x|
-      array[x], array[x + 1] = array[x + 1], array[x] if array[x] > array[x + 1]
+      array[x], array[x + 1] = array[x + 1], array[x] if array[x + 1].nil? == false && array[x] > array[x + 1]
     end
   end
   array
 end
 
-puts bubble_sort([4, 3, 78, 2, 0, 2])
+print bubble_sort([4, 3, 78, 2, 0, 2])
+puts
 
 def bubble_sort_by(array)
   n = array.length - 1
   n.times do
     0.upto(n) do |x|
-      array[x], array[x + 1] = array[x + 1], array[x] if yield(array[x], array[x + 1]) == 1
+      array[x], array[x + 1] = array[x + 1], array[x] if array[x + 1].nil? == false && yield(array[x], array[x + 1]) == 1
     end
   end
   print array
 end
 
-bubble_sort_by %w[hi hey hello] do |x, y|
+bubble_sort_by %w[hey hi hello] do |x, y|
   x.length <=> y.length
 end
 
