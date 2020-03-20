31c8d9013f5ffbcc161f3de9adee0386a460b154
aulbourn knowles <aulbourn@gmail.com>
2020-03-20 13:42:24 -0400

    started the bubble_sort_by method

--- main.rb
+++ main.rb
@@ -15,4 +15,13 @@ def bubble_sort(array)
 end
 
 arr = [4, 3, 78, 2, 0, 2]
-print bubble_sort(arr)
+# print bubble_sort(arr)
+
+def bubble_sort_by(array)
+  yield
+end
+
+string_array = ["hi", "hey", "hello"]
+yield_block = string_array.each do |a, b| a <=> b end
+
+bubble_sort_by($yield_block)
