129e6f4dd3576fd4b5ae3205a4ceeffcb552272a
Solomon APPIER-SIGN <braasig@gmail.com>
2020-03-21 00:04:41 +0000

    updated comparison

--- main.rb
+++ main.rb
@@ -16,7 +16,7 @@ def bubble_sort_by(array)
   n = array.length - 1
   n.times do
     0.upto(n) do |x|
-      if array[x + 1].nil? == false && yield(array[x], array[x + 1]) == 1
+      if array[x + 1].nil? == false && yield(array[x], array[x + 1]).positive?
         array[x], array[x + 1] = array[x + 1], array[x]
       end
     end
